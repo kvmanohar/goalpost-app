e8db0da968f2b14ead080369cbe1f4c0e21e980d
Manohar Kurapati <kvmanohar@gmail.com>
2017-11-27 22:22:31 +0000

    Completed creating createGoalVC and segue

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,40 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by Manohar Kurapati on 27/11/2017.
+//  Copyright © 2017 Manosoft. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    //MARK: IB Outlest
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    //MARK: IBActions
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
