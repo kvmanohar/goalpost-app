2aeeb6e4f6adb0e42c5264d1d355e6d8a77a4fb1
Manohar Kurapati <kvmanohar@gmail.com>
2017-11-29 20:19:56 +0000

    Created managedContext for CoreData

--- goalpost-app/Controller/FinishGoalVC.swift
+++ goalpost-app/Controller/FinishGoalVC.swift
@@ -28,8 +28,43 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
         createGoalBtn.bindToKeyboard()
     }
 
-    @IBAction func createGoalBtnPressed(_ sender: Any) {
+    //MARK: Local functions
+    func saveData(completion:(_ finished: Bool) -> ()){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            completion(true)
+            
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)" )
+            completion(false)
+        }
+        
     }
     
+    
+    //MARK: IBActions
 
+    @IBAction func createGoalBtnPressed(_ sender: Any) {
+        if pointsTextField.text != "" {
+            self.saveData { (complete) in
+                if complete {
+                    print("Data Saved successfully")
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }//EndOf CreateGoalBtnPressed
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
 }
