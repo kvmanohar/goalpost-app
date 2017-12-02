f1f859ad8c39858d922774e4444b2c953a1e4b98
Manohar Kurapati <kvmanohar@gmail.com>
2017-12-02 13:17:21 +0000

    Undo delete view successfully completed

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -16,6 +16,8 @@ class GoalsVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var undoView: UIView!
     
+
+    
     var goals: [Goal] = []
     
     override func viewDidLoad() {
@@ -23,7 +25,7 @@ class GoalsVC: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isHidden = false
-        undoView.isHidden = true
+        undoView.alpha = 0.0
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -61,10 +63,9 @@ class GoalsVC: UIViewController {
         fetchCoreDataObjects()
         tableView.reloadData()
         
-        UIView.animate(withDuration: 0.10) {
-            self.undoView.isHidden = true
+        UIView.animate(withDuration: 0.5) {
+            self.undoView.alpha = 0.0
         }
-        
     }//EndOf-undoDeleteBtnPressed
     
 }
@@ -90,6 +91,11 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.undoView.alpha = 0.0
+        let _ = saveGoalsDB()
+    }
+    
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
@@ -99,18 +105,28 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
         let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath)
             in
             self.removeGoal(atIndexPath: indexPath)
             self.fetchCoreDataObjects()
             tableView.deleteRows(at: [indexPath], with: .automatic)
-            
-            self.undoView.isHidden = false
+
+            UIView.animate(withDuration: 0.5, animations: {
+                self.undoView.alpha = 1.0
+            })
         }
         deleteAction.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
         
         let addAction = UITableViewRowAction(style: .normal, title: "ADD 1") { (rowAction, indexPath)
             in
+            
+            self.undoView.alpha = 0.0
+            if self.saveGoalsDB() != nil {
+                self.fetchCoreDataObjects()
+                tableView.reloadData()
+            }
+            
             self.setProgress(atIndexPath: indexPath)
             tableView.reloadRows(at: [indexPath], with: .automatic)
         }
@@ -138,19 +154,31 @@ extension GoalsVC {
         } catch {
             debugPrint("Could not set progress: \(error.localizedDescription)")
         }
-    }//EndOf-SetGoal
+    }//EndOf-SetProgress
+    
     
     func removeGoal(atIndexPath indexPath: IndexPath){
+        
         guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let _ = saveGoalsDB()
+        
         managedContext.delete(goals[indexPath.row])
-//        do {
-//            try managedContext.save()
-//            print("Successfully removed goal!")
-//        } catch {
-//            debugPrint("Could not remove item : \(error.localizedDescription)")
-//        }
         
     }//EndOf-removeGoal
+    
+    func saveGoalsDB() -> Bool?  {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return nil}
+        if managedContext.hasChanges {
+            do {
+                try managedContext.save()
+            } catch {
+                debugPrint("Could not save the previous delete : \(error.localizedDescription)")
+            }
+            return true
+        } else {
+            return nil
+        }
+    }//EndOf-saveGoalsDB
     
     func fetch(completion: (_ complete: Bool)->()){
         guard  let managedContext = appDelegate?.persistentContainer.viewContext else {  return }
